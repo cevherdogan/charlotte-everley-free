53c6b8fd83fada1a0caf54a1c5be9e1ad5fc2739
Cevher Dogan <cevherd@gmail.com>
2025-07-15 03:54:22 -0400

    🔄 Update and enhancements: - Added devon.html under articles with finalized layout and link structure - Converted articles/index.html to index.md for markdown flexibility - Included new infographic asset: assets/cultural-map.svg - Created zdocs/index.md with GPT redirect and setup reference - Updated README.md with new FSBO and contributor references - Synced assets (about-poster.png) and sitemap/robots for public/SEO structure

---
+++ ops/scripts/replace_email_contact.sh
@@ -0,0 +1,5 @@
+find . -type f -name "*.html" -exec sed -i '' 's/charlotteeverley@charlotteeverley\.previewhub-dev\.com/charlotte.everley@previewhub-dev.com/g' {} +
+
+
+# hello@charlotteeverley.com
+find . -type f -name "*.html" -exec sed -i '' 's/hello@charlotteeverley.com/charlotte.everley@previewhub-dev.com/g' {} +
